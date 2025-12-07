7c86c83835fbe10d42b0245d1c869d384082b40e
Harsha6404 <151925032+Harsha6404@users.noreply.github.com>
2025-12-07 10:34:24 +0530

    Update server.js

--- app/server.js
+++ app/server.js
@@ -1,6 +1,5 @@
-// Simple Node.js HTTP server
 const http = require('http');
-const port = process.env.PORT || 8080;
+const port = 8080;
 
 
 const server = http.createServer((req, res) => {
@@ -11,14 +10,12 @@ return;
 }
 if (req.url === '/') {
 res.writeHead(200, {'Content-Type': 'text/plain'});
-res.end('Hello from one-click API!');
+res.end('Hello from Terraform Provisioner version!');
 return;
 }
-res.writeHead(404, {'Content-Type': 'text/plain'});
+res.writeHead(404);
 res.end('not found');
 });
 
 
-server.listen(port, () => {
-console.log(`Server running on port ${port}`);
-});
+server.listen(port, () => console.log("Server running on port", port));
